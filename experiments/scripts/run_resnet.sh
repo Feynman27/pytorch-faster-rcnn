ad6cefd67861cb34d06180d07ba979fd8aef82f1
Thomas Balestri <thomas.balestri4@gmail.com>
2017-12-14 21:54:30 -0500

    add make_modules and run_resnet scripts

---
+++ experiments/scripts/run_resnet.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+CUDA_LAUNCH_BLOCKING=1 CUDA_VISIBLE_DEVICES=0 python ./tools/trainval_net.py \
+--weight data/pretrained_models/res101.pth \
+--imdb voc_2007_trainval \
+--imdbval voc_2007_test \
+--iters 100000 \
+--cfg experiments/cfgs/res101.yml \
+--net res101 \
+--set ANCHOR_SCALES [8,16,32] ANCHOR_RATIOS [0.5,1.0,2.0]
---
+++ make_modules.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+cd lib/layer_utils/roi_pooling/src/cuda
+echo "Compiling roi_pooling kernels by nvcc..."
+nvcc -c -o roi_pooling_kernel.cu.o roi_pooling_kernel.cu -x cu -Xcompiler -fPIC -arch=sm_52
+cd ../../
+python build.py
+cd ../../../
+
+cd lib/nms/src/cuda
+echo "Compiling nms kernels by nvcc..."
+nvcc -c -o nms_kernel.cu.o nms_kernel.cu -x cu -Xcompiler -fPIC -arch=sm_52
+cd ../../
+python build.py
+cd ../../
